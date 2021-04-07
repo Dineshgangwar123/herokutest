8362f74c381db5d0909daa0207112ef0a43efc33
Dineshgangwar123 <59816601+Dineshgangwar123@users.noreply.github.com>
2021-04-07 18:58:09 +0530

    first commit

--- src/main/java/com/springsqlproject/demo/TestClass.java
+++ src/main/java/com/springsqlproject/demo/TestClass.java
@@ -1,13 +1,13 @@
 package com.springsqlproject.demo;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
